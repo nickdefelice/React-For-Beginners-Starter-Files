323a311e747fa2f0a12c714c82407d3840fae7de
Nicholas Defelice <nick.d@threesixtyeight.is>
2019-01-02 13:20:44 -0600

    Lesson <= 7

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Order extends React.Component{
+    render() {
+        return (
+            <div>
+                Order Comp
+            </div>
+        )
+    }
+}
+
+export default Order
